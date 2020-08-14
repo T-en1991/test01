21cd38fc4beff452c0567b4961436c00b25fed31
T-en1991 <940075665@qq.com>
2020-08-14 17:30:27 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,132 +1,109 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-
-    <child>
-      <template slot-scope="user">
-        {{user.data}}
-      </template>
-    </child>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs11
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    经度：
+    <el-input style="width: 200px;" v-model="a"></el-input>
+    纬度：
+    <el-input style="width: 200px;" v-model="b"></el-input>
+    <el-button @click="btn">111</el-button>
+    <baidu-map style="width: 100%;height: 800px" :center="center" class="map" :zoom="zoom" :scroll-wheel-zoom="true">
+      <bm-marker :position="{lng: 11, lat: 11}" scroll-wheel-zoom="true"
+                 :icon="{url: '../../static/greenGuo.png', size: {width: 100, height: 100},imageSize:'100%'}">
+      </bm-marker>
+      <bm-marker :position="{lng: 22, lat: 22}" scroll-wheel-zoom="true"
+                 :icon="{url: 'http://developer.baidu.com/map/jsdemo/img/fox.gif', size: {width: 300, height: 157}}">
+      </bm-marker>
+      <bm-marker :position="{lng: 33, lat: 33}" scroll-wheel-zoom="true"
+                 :icon="{url: 'http://developer.baidu.com/map/jsdemo/img/fox.gif', size: {width: 300, height: 157}}">
+      </bm-marker>
+      <bm-marker :position="{lng: 44, lat: 44}" scroll-wheel-zoom="true"
+                 :icon="{url: 'http://developer.baidu.com/map/jsdemo/img/fox.gif', size: {width: 300, height: 157}}">
+      </bm-marker>
+      <bm-overlay
+        pane="markerPane"
+        style="width:200px;height: 120px;background-color: rgba(0,0,0,.4);"
+        @mouseover.native="active = true"
+        @mouseleave.native="active = false">
+        <el-row>
+          <el-col :span="16">
+            某某某监测站
+          </el-col>
+          <el-col :span="8" style="color: white">
+            <i class="el-icon-edit"></i>
+            <i class="el-icon-close" @click="test"></i>
+          </el-col>
+          <el-divider></el-divider>
+          <el-row style="color: white">
+            <el-col :span="8">
+              二级
+            </el-col>
+            <el-col :span="16">
+              <el-col>
+                站点位置
+              </el-col>
+              <el-col>
+                站点类型
+              </el-col>
+              <el-col>
+                考核目标
+              </el-col>
+            </el-col>
+          </el-row>
+        </el-row>
+      </bm-overlay>
+      <bm-local-search :auto-viewport="true" :location="center"></bm-local-search>
+    </baidu-map>
   </div>
 </template>
 
 <script>
-  import child from './child'
   export default {
     name: 'HelloWorld',
     data() {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        center: {lng: 11, lat: 11},
+        zoom: 10,
+        a: 0,
+        b:0,
+        location: '北京',
+        keyword: '百度',
+        infoWindow: {
+          show: true,
+          contents: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        }
       }
     },
-    components:{
-      child
-    },
-    mounted() {
-      console.log(this.$axios);
-      this.$axios.get('http://192.168.1.26/mock/1621/platform/v1.0.0/dict/type/%7BdictId%7D').then(data => {
-        console.log(data);
-      })
+    methods: {
+      infoWindowClose(e) {
+        this.infoWindow.show = false
+      },
+      infoWindowOpen(e) {
+        this.infoWindow.show = true
+      },
+      clear() {
+        this.infoWindow.contents = ''
+      },
+      btn() {
+        console.log(this.a, this.b);
+
+        this.$set(this.center, 'lng', this.a)
+        this.$set(this.center, 'lat', this.b)
+        this.zoom = 10;
+      },
+      test(){
+        console.log(1);
+      }
     }
+
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1, h2 {
-    font-weight: normal;
+  .bm-view {
+    width: 100%;
+    height: 500px;
   }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
+.disN{
+  display: none;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import router from './router'
 import axios from 'axios';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+
+import BaiduMap from 'vue-baidu-map';
+Vue.use(BaiduMap, {
+  ak: 'HM3hamGnrbh1n3HfCLePRlfP4FUGRKEL'
+})
+
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 Vue.use(ElementUI);
